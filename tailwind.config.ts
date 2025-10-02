57088e10a99dcfeaf9e13f9ae66d6d0f4a5fe91b
yahwr <samy.sal.08@gmail.com>
2025-10-01 20:03:20 -0400

    update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,59 +6,6 @@ const config: Config = {
         './components/**/*.{js,ts,jsx,tsx,mdx}',
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
-    theme: {
-        extend: {
-            // 1. Define tus keyframes
-            keyframes: {
-                'fade-in-a': {
-                    '0%': { opacity: '0' },
-                    '100%': { opacity: '1' },
-                },
-                'fade-in-down-a': {
-                    '0%': {
-                        opacity: '0',
-                        transform: 'translateY(-20px)',
-                    },
-                    '100%': {
-                        opacity: '1',
-                        transform: 'translateY(0)',
-                    },
-                },
-                'fade-in-left-a': {
-                    '0%': {
-                        opacity: '0',
-                        transform: 'translateX(-20px)',
-                    },
-                    '100%': {
-                        opacity: '1',
-                        transform: 'translateX(0)',
-                    },
-                },
-                'subtle-pulse-a': {
-                    '0%, 100%': { transform: 'scale(1)' },
-                    '50%': { transform: 'scale(1.05)' },
-                },
-                'fade-in-up-a': {
-                    '0%': {
-                        opacity: '0',
-                        transform: 'translateY(20px)',
-                    },
-                    '100%': {
-                        opacity: '1',
-                        transform: 'translateY(0)',
-                    },
-                },
-            },
-            // 2. Define la clase 'animation' al mismo nivel que 'keyframes'
-            animation: {
-                'fade-in': 'fade-in-a 0.3s ease-in-out',
-                'fade-in-up': 'fade-in-up-a 0.5s ease-in-out',
-                'fade-in-down': 'fade-in-down-a 0.5s ease-in-out',
-                'fade-in-left': 'fade-in-left-a 0.5s ease-in-out',
-                'subtle-pulse': 'subtle-pulse-a 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-            },
-        },
-    },
     plugins: [],
 };
 

74d7bfbceafcab951bc78d8e136aaf5c3437fd13
yahwr <samy.sal.08@gmail.com>
2025-10-14 14:24:18 -0400

    change the projectStatus order

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -1,10 +1,10 @@
 import { ProjectStatus, TaskStatus } from "./types"
 
 export const PROJECT_STATUS_MAP: Readonly<Record<ProjectStatus, string>> = {
-    in_progress: 'En progreso',
-    completed: 'Completado',
     pending: 'Pendiente',
+    in_progress: 'En progreso',
     in_pause: 'En pausa',
+    completed: 'Completado',
 }
 
 export const PROJECT_STATUS_ARRAY: ProjectStatus[] = Object.keys(PROJECT_STATUS_MAP) as ProjectStatus[]
